66c40eddf7bdc5c38473c65b642e36a9d6dbb88d
Belanir <vadim-vadim15@rambler.ru>
2020-12-06 18:58:15 +0500

    Aside+body

---
+++ frontend/src/components/aside/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import './index.scss'
+
+const Aside = () =>{
+
+    return (
+        <aside className="aside">
+            fffffffffffffffff
+        </aside>
+    );
+}
+
+export default Aside;
